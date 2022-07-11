f37be3d09e29adf4f0453f0251fd7ad3c443119c
luian alves <luian.alves@gmail.com>
2022-07-11 11:59:25 -0300

    configurando arquivo htaccess

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -77,12 +77,5 @@
                 </a>
             </li>
         @endif
-
-        <li class="menu-item">
-            <a href="{{route('chart.despesa')}}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-building"></i>
-                <div data-i18n="Support">Charts</div>
-            </a>
-        </li>
     </ul>
 </aside>
