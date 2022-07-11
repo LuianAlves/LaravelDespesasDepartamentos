a05d56557dcae9b1bad0a14236dddff56bf2a176
luian alves <luian.alves@gmail.com>
2022-07-11 14:52:43 -0300

    removendo rota de registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,34 @@
 @extends('layouts.layout_auth')
 
 @section('content_auth')
-    <div class="card">
-        <div class="card-body">
+<div class="card">
+    <div class="card-body">
 
         <!-- /Logo -->
         <h4 class="mb-2 text-center">Sistema Orçamentário 🤑</h4>
         <p class="mb-4 text-center">Faça Login para iniciar seu Orçamento !!</p>
 
-        <form id="formAuthentication" class="mb-3" action="{{url('https://bgorcamento.herokuapp.com/login')}}" method="POST">
+        <form id="formAuthentication" class="mb-3" action="{{url('https://bgorcamento.herokuapp.com/login')}}"
+            method="POST">
             @csrf
 
             <div class="mb-3">
                 <label for="login" class="form-label">E-mail ou Usuário</label>
-                <input type="text" class="form-control" id="login" name="login" :value="old('login')" placeholder="Entre com seu email ou usuário" autofocus/>
+                <input type="text" class="form-control" id="login" name="login" :value="old('login')"
+                    placeholder="Entre com seu email ou usuário" autofocus />
             </div>
 
             <div class="mb-3 form-password-toggle">
                 <div class="d-flex justify-content-between">
                     <label class="form-label" for="password">Senha</label>
                     <a href="{{ route('password.request') }}">
-                    <small>Esqueceu a senha?</small>
+                        <small>Esqueceu a senha?</small>
                     </a>
                 </div>
                 <div class="input-group input-group-merge">
-                    <input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" autocomplete="current-password" />
+                    <input type="password" id="password" class="form-control" name="password"
+                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                        aria-describedby="password" autocomplete="current-password" />
                     <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                 </div>
             </div>
@@ -40,12 +44,6 @@
             </div>
         </form>
 
-        <p class="text-center">
-            <span>Ainda não tem registro?</span>
-            <a href="{{route('register')}}">
-                <span>Registrar nova conta</span>
-            </a>
-        </p>
-        </div>
     </div>
+</div>
 @endsection
